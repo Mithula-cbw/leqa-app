89c3c3e7bea88db7e7d99a7eb0c13122758e8952
Mithula Chanthuka <MITHULA.cbw@gmail.com>
2025-12-30 07:22:18 +0530

    destruct: delete unnecessary coponents

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,13 +2,11 @@
 
 export { default as AlertDialog } from "./AlertDialog";
 export { default as Avatar } from "./Avatar";
-export { default as ExpiryPicker } from "./ExpiryPicker";
 export { default as NewUserAppearanceCard } from "./NewUserAppearanceCard";
 export { default as NewUserNameCard } from "./NewUserNameCard";
 export { default as NewUserThemeButton } from "./NewUserThemeButton";
 export { default as NoProductsFound } from "./NoProductsFound";
 export { default as ProfileImagePicker } from "./ProfileImagePicker";
-export { default as QuantityPicker } from "./QuantityPicker";
 export { default as ReductionModal } from "./ReductionModal";
 export { default as ThemedText } from "./themed-text";
 export { default as ThemedView } from "./themed-view";
